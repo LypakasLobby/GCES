ed3dcd3f98f5672e6a701c417c3da8d8a9a8fb1e
Lypaka <ryan96t@gmail.com>
2025-11-01 18:56:04 -0500

    10.3.3

--- src/main/java/com/lypaka/gces/Listeners/EXPListener.java
+++ src/main/java/com/lypaka/gces/Listeners/EXPListener.java
@@ -18,24 +18,33 @@ public class EXPListener {
         CobblemonEvents.EXPERIENCE_GAINED_EVENT_PRE.subscribe(Priority.NORMAL, event -> {
 
             Pokemon pokemon = event.getPokemon();
-            ServerPlayerEntity player = pokemon.getOwnerPlayer();
-            if(player == null){
-                return Unit.INSTANCE;
-            }
-            String diff = ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Difficulty");
-            if (diff.equalsIgnoreCase("none")) return Unit.INSTANCE;
-            Difficulty difficulty = GCES.difficultyMap.get(diff);
-            LevelingModule levelingModule = difficulty.getLevelingModule();
-            int tierLevel = Integer.parseInt(ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Leveling"));
-            int maxLevel = levelingModule.getTierMap().get("Tier-" + tierLevel);
-            int pokemonLevel = pokemon.getLevel();
-            int experience = event.getExperience();
-            int experienceRequired = pokemon.getExperienceToLevel(maxLevel+1);
-            if (pokemonLevel >= maxLevel || experience >= experienceRequired) {
+            if (pokemon.isPlayerOwned()) {
 
-                event.cancel();
-                event.setExperience(Math.max(experienceRequired - 1,0));
-                player.sendMessage(FancyTextHandler.getFormattedText(ConfigGetters.levelingTierMessage));
+                ServerPlayerEntity player = pokemon.getOwnerPlayer();
+                if (player == null) {
+
+                    GCES.logger.info("[GCES] An error was detected trying to get a player owner from a Pokemon. Printing debug info.");
+                    GCES.logger.info(pokemon.getPersistentData());
+                    GCES.logger.info("Player owned? " + pokemon.isPlayerOwned());
+                    return Unit.INSTANCE;
+
+                }
+                String diff = ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Difficulty");
+                if (diff.equalsIgnoreCase("none")) return Unit.INSTANCE;
+                Difficulty difficulty = GCES.difficultyMap.get(diff);
+                LevelingModule levelingModule = difficulty.getLevelingModule();
+                int tierLevel = Integer.parseInt(ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Leveling"));
+                int maxLevel = levelingModule.getTierMap().get("Tier-" + tierLevel);
+                int pokemonLevel = pokemon.getLevel();
+                int experience = event.getExperience();
+                int experienceRequired = pokemon.getExperienceToLevel(maxLevel+1);
+                if (pokemonLevel >= maxLevel || experience >= experienceRequired) {
+
+                    event.cancel();
+                    event.setExperience(Math.max(experienceRequired - 1, 0));
+                    player.sendMessage(FancyTextHandler.getFormattedText(ConfigGetters.levelingTierMessage));
+
+                }
 
             }
 
--- src/main/java/com/lypaka/gces/Listeners/SpawnListener.java
+++ src/main/java/com/lypaka/gces/Listeners/SpawnListener.java
@@ -17,22 +17,27 @@ public class SpawnListener {
     public static void register() {
 
         CobblemonEvents.POKEMON_ENTITY_SPAWN.subscribe(Priority.NORMAL, event -> {
-            if (!ConfigGetters.scalePokemonSpawns)
-                return Unit.INSTANCE;
-            PokemonEntity entity = event.getEntity();
-            if (event.getCtx().getCause().getEntity() instanceof ServerPlayerEntity) {
-                ServerPlayerEntity player = (ServerPlayerEntity) event.getCtx().getCause().getEntity();
-                if (player != null) {
 
-                    String diff = ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Difficulty");
-                    if (diff.equalsIgnoreCase("none")) return Unit.INSTANCE;
-                    Difficulty difficulty = GCES.difficultyMap.get(diff);
-                    CatchingModule module = difficulty.getCatchingModule();
-                    int tierNum = Integer.parseInt(ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Catching"));
-                    int maxLevel = module.getTierMap().get("Tier-" + tierNum);
-                    String scale = ConfigGetters.pokemonSpawnScale;
-                    Pokemon pokemon = entity.getPokemon();
-                    pokemon.setLevel(Utils.getNewLevel(maxLevel, scale));
+            if (!ConfigGetters.scalePokemonSpawns) return Unit.INSTANCE;
+            PokemonEntity entity = event.getEntity();
+            Pokemon pokemon = entity.getPokemon();
+            if (!pokemon.isPlayerOwned()) {
+
+                if (event.getCtx().getCause().getEntity() instanceof ServerPlayerEntity) {
+
+                    ServerPlayerEntity player = (ServerPlayerEntity) event.getCtx().getCause().getEntity();
+                    if (player != null) {
+
+                        String diff = ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Difficulty");
+                        if (diff.equalsIgnoreCase("none")) return Unit.INSTANCE;
+                        Difficulty difficulty = GCES.difficultyMap.get(diff);
+                        CatchingModule module = difficulty.getCatchingModule();
+                        int tierNum = Integer.parseInt(ConfigGetters.playerAccountsMap.get(player.getUuid().toString()).get("Catching"));
+                        int maxLevel = module.getTierMap().get("Tier-" + tierNum);
+                        String scale = ConfigGetters.pokemonSpawnScale;
+                        pokemon.setLevel(Utils.getNewLevel(maxLevel, scale));
+
+                    }
 
                 }
 
